89c0de5472a2766f3d543b37a263fd7a92aa263c
sondxz <hoangsonnguyen204@gmail.com>
2025-12-26 14:42:07 +0700

    feat: Chapter 05 video 003

--- src/main/java/vn/hoangson/pickerballshop/PickerballApplication.java
+++ src/main/java/vn/hoangson/pickerballshop/PickerballApplication.java
@@ -2,12 +2,19 @@ package vn.hoangson.pickerballshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class PickerballApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PickerballApplication.class, args);
+
+		//container
+		ApplicationContext hoangson = SpringApplication.run(PickerballApplication.class, args);
+		for(String s: hoangson.getBeanDefinitionNames())
+		{
+			System.out.println(s);
+		}
 	}
 
 }
