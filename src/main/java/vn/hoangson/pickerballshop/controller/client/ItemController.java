8bf4b6454b284d98d3c2c7ad58a306006c89a863
sondxz <hoangsonnguyen204@gmail.com>
2026-01-04 15:20:00 +0700

    feat: Chapter 12 video 002 + 003

---
+++ src/main/java/vn/hoangson/pickerballshop/controller/client/ItemController.java
@@ -0,0 +1,17 @@
+package vn.hoangson.pickerballshop.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import ch.qos.logback.core.model.Model;
+
+
+@Controller
+public class ItemController {
+    @GetMapping("/product/{id}")
+    public String getProductPage(Model model, @PathVariable long id) {
+        return "client/product/detail";
+    }
+    
+}
